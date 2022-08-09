a933e680bac678401ac3122b5c65de952098b1ce
Sven Wanner <sven.wanner@gmail.com>
2022-08-09 21:21:14 +0200

    pytest.ini added

--- setup.py
+++ setup.py
@@ -1,44 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""The setup script."""
-
-from setuptools import setup, find_packages
-
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
-# TODO: add install requirements
-requirements = ['numpy', 'Pillow']
-
-setup_requirements = ['pytest-runner', ]
-
-test_requirements = ['pytest', ]
-
-# TODO: add console scripts
-entry_points = {
-    'console_scripts': [],
-}
+from distutils.core import setup
 
 setup(
-    author='artificialpixels',
-    classifiers=[
-        'Programming Language :: Python :: 3.9',
-    ],
-    description='Calibration Toolbox',
-    entry_points=entry_points,
-    python_requires='>=3.9',
-    install_requires=requirements,
-    license='Not open source',
-    long_description=readme,
-    keywords='calibpy',
-    name='calibpy',
-    packages=find_packages(exclude=['tests']),
-    setup_requires=setup_requirements,
-    test_suite='test',
-    tests_require=test_requirements,
-    url='https://github.com/artificialpixels/calibpy',
+    name='Calibpy',
     version='0.0.1',
-    zip_safe=False,
-    package_dir={'calibpy': 'calibpy'},
+    packages=['towelstuff'],
+    license='propretary',
+    long_description=open('README.md').read(),
 )
